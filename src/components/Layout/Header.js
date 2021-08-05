671a295a9275010f8249d58186264d87110b3ab1
Chef NoName <abdijama21@gmail.com>
2021-08-05 01:09:42 +0100

    update

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,4 +1,5 @@
 import { Fragment } from "react";
+import HeaderCartBtn from "./HeaderCartBtn";
 
 import classes from "./Header.module.css";
 import buffetImage from "../../assets/buffet.jpg";
@@ -8,7 +9,7 @@ const Header = (props) => {
     <Fragment>
       <header className={classes.header}>
         <h1>camelCaseCaf&egrave;</h1>
-        <button>Basket</button>
+        <HeaderCartBtn />
       </header>
       <div className={classes["main-image"]}>
         <img src={buffetImage} alt="A table of some BANGING! food" />
