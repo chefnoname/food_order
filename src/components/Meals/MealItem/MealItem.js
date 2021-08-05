79d70ee7707e31cdecaf9db8e4354093a9a9c478
Chef NoName <abdijama21@gmail.com>
2021-08-05 02:53:36 +0100

    update

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,3 +1,4 @@
+import MealItemForm from "./MealItemForm";
 import classes from "./MealItem.module.css";
 
 const MealItem = (props) => {
@@ -9,7 +10,7 @@ const MealItem = (props) => {
         <div className={classes.description}>{props.description}</div>
         <div className={classes.price}>{price}</div>
       </div>
-      <div></div>
+      <MealItemForm />
     </li>
   );
 };
