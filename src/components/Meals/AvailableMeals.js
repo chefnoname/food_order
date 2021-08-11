657c98c8e13e25ee653a04b5bce2f3612f42eaa3
Chef NoName <abdijama21@gmail.com>
2021-08-11 15:59:18 +0100

    update

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,36 +1,13 @@
+import { useEffect } from "react";
 import Card from "../UI/Card";
 import classes from "./AvailableMeals.module.css";
 import MealItem from "./MealItem/MealItem";
 
-const DUMMY_MEALS = [
-  {
-    id: "m1",
-    name: "Suqaar",
-    description: "Finest cubes of meat slow cooked in a stew for 2 hours",
-    price: 22.99,
-  },
-  {
-    id: "m2",
-    name: "Bariis & Hleb",
-    description: "A Somali specialty!",
-    price: 16.5,
-  },
-  {
-    id: "m3",
-    name: "Canjeero",
-    description:
-      "Thin Somali pankcakes - served with a portion of Suqaar or Shaax",
-    price: 12.99,
-  },
-  {
-    id: "m4",
-    name: "Mushaari",
-    description: "Healthy traditional Somali porridge",
-    price: 18.99,
-  },
-];
-
 const AvailableMeals = () => {
+  useEffect(() => {
+    fetch("https://camelcasecafe-default-rtdb.firebaseio.com/meals.json");
+  }, []);
+
   const mealsList = DUMMY_MEALS.map((meal) => (
     <MealItem
       id={meal.id}
